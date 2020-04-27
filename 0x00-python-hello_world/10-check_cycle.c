9b090deabf421d34c0e6d33b6beea67398acee14
samisosa20 <1449@holbertonschool.com>
2020-04-27 16:19:00 +0000

    0x00-python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * check_cycle - function in C that checks if a singly
+ * linked list has a cycle in i
+ * @list: pointer to list to be freed
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	
+}
