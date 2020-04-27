fa82be5e322973ee5670319967b0e36006716da0
samisosa20 <1449@holbertonschool.com>
2020-04-27 16:21:31 +0000

    add code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,5 +7,22 @@
  */
 int check_cycle(listint_t *list)
 {
-	
+		listint_t *head;
+	listint_t *tail;
+
+	if (list == NULL)
+		return (0);
+
+	head = list;
+	tail = list;
+
+	while (tail != NULL && tail->next != NULL)
+	{
+		head = head->next;
+		tail = tail->next->next;
+
+		if (head == tail)
+			return (1);
+	}
+	return (0);
 }
