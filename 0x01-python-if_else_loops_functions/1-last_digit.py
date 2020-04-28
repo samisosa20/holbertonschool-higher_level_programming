ae665fdf482c69b3424bed5057d316dbcad1a224
samisosa20 <1449@holbertonschool.com>
2020-04-28 18:23:11 +0000

    add conditional if negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    number = number * -1
-if (number % 10 < 6 and number % 10 > 0):
+    last = (number * -1) % 10
+else:
+    last = number % 10
+if (last < 6 and last > 0):
     str = "and is less than 6 and not 0"
-elif (number % 10 > 5):
+elif (last > 5):
     str = "and is greater than 5"
 else:
     str = "and is 0"
-print("Last digit of {} is {} {}".format(number, number % 10, str))
+print("Last digit of {} is {} {}".format(number, last, str))
