3d65c313f631d2ea9227741b130c08238975341a
samisosa20 <1449@holbertonschool.com>
2020-04-28 18:56:15 +0000

    fix request

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+string = ""
 for str in range(122, 96, -1):
     if (str % 2 != 0):
-        str = str - 32
-    print(chr(str), end='')
+        string += chr(str - 32)
+    else:
+        string += chr(str)
+print(string, end='')
