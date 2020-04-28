c030f7cf819ac24959fa1a0644c1f5e0203f58e9
samisosa20 <1449@holbertonschool.com>
2020-04-28 18:25:08 +0000

    change conditional > 0 to !=

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    last = (number * -1) % 10
+    last = (number * -1) % 10 * -1
 else:
     last = number % 10
-if (last < 6 and last > 0):
+if (last < 6 and last != 0):
     str = "and is less than 6 and not 0"
 elif (last > 5):
     str = "and is greater than 5"
