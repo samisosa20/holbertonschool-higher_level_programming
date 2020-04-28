a7ee5f01cc7dce2468f7228d14fd55ae2b000ef9
samisosa20 <1449@holbertonschool.com>
2020-04-28 18:31:27 +0000

    fix pep8

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for str in range(122, 96, -1):
     if (str % 2 != 0):
-        print(chr(str - 32), end = '')
+        print(chr(str - 32), end='')
     else:
-        print(chr(str), end = '')
+        print(chr(str), end='')
