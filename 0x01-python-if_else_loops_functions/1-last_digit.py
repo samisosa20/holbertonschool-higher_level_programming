9ea194c55e97c889d1ccd67215045aaf1c10fea9
samisosa20 <1449@holbertonschool.com>
2020-04-28 18:20:30 +0000

    add conditional if negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if (number < 0):
+    number = number * -1
 if (number % 10 < 6 and number % 10 > 0):
     str = "and is less than 6 and not 0"
 elif (number % 10 > 5):
