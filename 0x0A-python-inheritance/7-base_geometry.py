d7b7253f262185a6978a243efccd3a5b6ecd9e7c
samisosa20 <1449@holbertonschool.com>
2020-06-02 23:44:05 +0000

    fix code

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""module: 7-base_geometry"""
+"""Documentation for BaseGeometry class"""
 
 
-class BaseGeometry():
-    """class BaseGeometry"""
-    def __init__(self):
-        pass
+class BaseGeometry:
+
+    """Base Geometry class that is empty"""
 
     def area(self):
+        """Area function for geometry"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
+        """Validates an integer value
+        Args:
+            name (str): the string name
+            value (int): the value to be validated
+        Raises:
+            TypeError: if the value is not an integer
+            ValueError: if the value is less than or equal to 0
+        """
+
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,33 +2,43 @@
 """module: 8-base_geometry"""
 
 
-class BaseGeometry():
-    """class BaseGeometry"""
-    def __init__(self):
-        pass
+class BaseGeometry:
+
+    """Base Geometry class that is empty"""
 
     def area(self):
+        """Area function for geometry"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
+        """Validates an integer value
+        Args:
+            name (str): the string name
+            value (int): the value to be validated
+        Raises:
+            TypeError: if the value is not an integer
+            ValueError: if the value is less than or equal to 0
+        """
+
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if (value <= 0):
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
+
+    """Rectangle class that inherits from BaseGeometry class"""
+
     def __init__(self, width, height):
+        """Instantiation function
+        Args:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+        """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """method: area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """method: str"""
-        s = "[Rectangle] {}/{}".format(self.__width, self.__height)
-        return s
