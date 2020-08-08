0c623c35584c9edf12b04b91fcfc50bea668e42c
samisosa20 <1449@holbertonschool.com>
2020-08-08 15:01:38 +0000

    fix point 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
-SELECT all data in the table
-states with first leter is N
+"""
+SQL Injection safe version of `2-my_filter_states.py`.
+Takes in an argument and displays all values in the `states`
+table of `hbtn_0e_0_usa` where `name` matches the argument.
 """
 
 if __name__ == '__main__':
