19eb13d8beb965878d9a531aaf8119ac3681f404
samisosa20 <1449@holbertonschool.com>
2020-08-08 15:11:25 +0000

    fix Requirement

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""Lists all the cities of a particular state from `hbtn_0e_4_usa` database."""
+"""
+python script that lists all cities from the database hbtn_0e_4_usa
+with specified state name
+"""
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb
+import MySQLdb
+from sys import argv
 
-    target_state = (argv[4], )
-
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute(("SELECT cities.id, cities.name "
-                    "FROM states JOIN cities "
-                    "ON states.id = cities.state_id "
-                    "WHERE states.name = %s "
-                    "ORDER BY cities.id ASC"), target_state)
-    cities = cursor.fetchall()
-    for city in cities:
-        print(city)
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
+    rows = cursor.fetchall()
+    print(", ".join(city[0] for city in rows))
     cursor.close()
     db.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-
-#!/usr/bin/python3
 """
-This script lists the first State object from database passed into program
-Using SQLalchemy, this script connects to a MySQL server running on
-localhost at port 3306.
-This script takes 3 arguments: mysql username,
-mysql password, and database name.
-These arguments are used to connect to the MySQL server.
+list all State objects that contain the letter a from a database
 """
 
-
-import sys
-from model_state import Base, State
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    a_states = session.query(State) \
-        .order_by(State.id.asc()) \
-        .filter(State.name.ilike('%a%')) \
-        .all()
-
-    for a_state in a_states:
-        print("{}: {}".format(a_state.id, a_state.name))
+    s = '%a%'
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
