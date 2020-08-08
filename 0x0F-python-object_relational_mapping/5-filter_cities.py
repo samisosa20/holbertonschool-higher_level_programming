d0caf191845c5d098efd85b04e783e44da419186
samisosa20 <1449@holbertonschool.com>
2020-08-08 15:09:13 +0000

    fix Requirement

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-''' SELECT all data in the table cities'''
+"""Lists all the cities of a particular state from `hbtn_0e_4_usa` database."""
 
 if __name__ == '__main__':
-    import sys
+    from sys import argv
     import MySQLdb
 
-    state = (sys.argv[4],)
+    target_state = (argv[4], )
+
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
             )
-
-    # you must create a Cursor object. It will let
-    #  you execute all the queries you need
-    cur = db.cursor()
-
-    cursor.execute(("SELECT cities.id, cities.name \
-                FROM states JOIN cities \
-                ON states.id = cities.state_id \
-                WHERE states.name = %s \
-                ORDER BY cities.id ASC"), state)
+    cursor = db.cursor()
+    cursor.execute(("SELECT cities.id, cities.name "
+                    "FROM states JOIN cities "
+                    "ON states.id = cities.state_id "
+                    "WHERE states.name = %s "
+                    "ORDER BY cities.id ASC"), target_state)
     cities = cursor.fetchall()
     for city in cities:
         print(city)
