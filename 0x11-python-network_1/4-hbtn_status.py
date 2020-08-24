01adaf81dd42f9406d79b9bf6d1693c8741b0e11
Sammy <1449@holbertonschool.com>
2020-08-24 13:29:17 -0500

    0x11-network

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
