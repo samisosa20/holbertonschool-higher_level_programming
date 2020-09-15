48e4d57a1000cfc34884700b5ee9d03de06ce8b8
Sammy <1449@holbertonschool.com>
2020-09-15 14:01:37 -0500

    fix ponit 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  let i = 2;
-  while (process.argv[i]) {
-    console.log(process.argv[i]);
-    i++;
-  }
-} else {
-  console.log('No argument');
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
