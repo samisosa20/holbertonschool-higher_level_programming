d59e77a3943461e81c13f423a9596125bec7d802
Sammy <1449@holbertonschool.com>
2020-09-21 13:02:40 -0500

    add default value

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,10 +7,10 @@ class Rectangle {
     }
   }
 
-  print () {
+  print (c = 'X') {
     let i = 0;
     while (i < this.height) {
-      console.log('X'.repeat(this.width));
+      console.log(c.repeat(this.width));
       i++;
     }
   }
