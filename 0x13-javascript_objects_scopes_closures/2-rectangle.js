85839fb4c0f01834dda2547ee17469eed84a6e0d
Sammy <1449@holbertonschool.com>
2020-09-21 12:23:30 -0500

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0) {
       this.height = h;
       this.width = w;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0) {
       this.height = h;
       this.width = w;
     }
   }
 
-  print() {
+  print () {
     let i = 0;
     while (i < this.height) {
       console.log('X'.repeat(this.width));
