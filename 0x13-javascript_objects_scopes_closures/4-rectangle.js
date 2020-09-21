a08853ea0bd352ad515c36d613743521f0992b7b
Sammy <1449@holbertonschool.com>
2020-09-21 12:50:13 -0500

    change order atributes

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,8 +2,8 @@
 class Rectangle {
   constructor (width, height) {
     if (height > 0 && width > 0) {
-      this.height = height;
       this.width = width;
+      this.height = height;
     }
   }
 
