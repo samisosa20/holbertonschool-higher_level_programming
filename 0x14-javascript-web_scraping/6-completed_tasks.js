1e754c727af1d156baffc4134445ce65d5c5976a
Sammy <1449@holbertonschool.com>
2020-09-22 12:21:56 -0500

    fix point 6 add completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,18 +6,15 @@ request(url, function (error, response, body) {
   if (response.statusCode === 200) {
     const data = JSON.parse(body);
     const result = {};
-    let i = 0;
-    let aux = '';
     data.forEach(element => {
-      if (aux !== element.userId && aux !== '') {
-        result[aux] = i;
-        i = 1;
-      } else {
-        i++;
+      if (element.completed === true) {
+        if (result[element.userId]) {
+          result[element.userId]++;
+        } else {
+          result[element.userId] = 1;
+        }
       }
-      aux = element.userId;
     });
-    result[aux] = i;
     console.log(result);
   } else {
     console.error(error);
